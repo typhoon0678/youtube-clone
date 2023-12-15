43853c801daf411ca579d5e88606d8dfdef28946
DESKTOP-FAJVM5E\kth04 <kth0432@naver.com>
2023-12-15 13:26:40 +0900

    초기 세팅 및 헤더 추가

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css'
+import PageHeader from './layouts/PageHeader'
+
+function App() {
+  return <div className='max-h-screen flex flex-col'>
+    <PageHeader />
+    <div className='grid grid-cols-[auto,1fr] flex-grow-1 overflow-auto'>
+      
+    </div>
+  </div>
+}
+
+export default App
